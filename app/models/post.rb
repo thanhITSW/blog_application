d8af0a87c8a1f86d30af306ea23535848a5217b1
thanhITSW <nguyenmtthanh13@gmail.com>
2025-03-31 14:43:00 +0700

    add comment post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
   validates :title, presence: true, length: {minimum: 5, maximum: 50}
   belongs_to :user
+  has_many :comments, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,12 @@ Rails.application.routes.draw do
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
+
+  #/posts/1/comments/4
   get '/u/:id', to: 'users#profile', as: 'user'
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
   get 'about', to: 'pages#about' # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
